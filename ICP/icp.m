98239e23532f3ac3179f37da353510eed01b5baa
GiorgosMethe <giorgosmethe@gmail.com>
2014-05-02 18:34:31 +0200

    added stop criterion

--- ICP/icp.m
+++ ICP/icp.m
@@ -3,8 +3,9 @@ function[R, T] = icp(base, target)
 %% Initialize rotation, translation
 R = eye(3, 3);
 T = zeros(1, 3);
+rms = 0;
 
-for i=1:20
+for i=1:100
     %% New transformed target cloud
     base = (R * base(:,1:3)')' + repmat(T,size(base,1),1);
     
@@ -16,14 +17,32 @@ for i=1:20
     [R, T] = getTransformation(target, base);
     
     %% Find Rms between base and target
+    oldRms = rms;
     rms = getRms(base, target);
-    
-    p=base';
-    p1=target';
-    figure
-    hold on
-    title(rms)
-    scatter3(p(1,:),p(2,:),p(3,:),'bo');
-    scatter3(p1(1,:),p1(2,:),p1(3,:),'r+');
-    hold off
+    if (abs(oldRms - rms) < 10e-15)
+        rms
+        break
+    end
+    %% show initial pointclouds
+    if(i == 1)
+        p=base';
+        p1=target';
+        figure
+        hold on
+        title(rms)
+        scatter3(p(1,:),p(2,:),p(3,:),'bo');
+        scatter3(p1(1,:),p1(2,:),p1(3,:),'r+');
+        hold off
+    end
+end
+%% show final pointclouds
+p=base';
+p1=target';
+figure
+hold on
+title(rms)
+scatter3(p(1,:),p(2,:),p(3,:),'bo');
+scatter3(p1(1,:),p1(2,:),p1(3,:),'r+');
+hold off
+
 end
