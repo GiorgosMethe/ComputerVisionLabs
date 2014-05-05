bc0d912a025f724f5def6fa178ed4830fbfdba6c
GiorgosMethe <giorgosmethe@gmail.com>
2014-05-05 20:48:03 +0200

    final version - results not yet there

--- ICP/main.m
+++ ICP/main.m
@@ -15,11 +15,14 @@ for frame = 2:2:98
     target = cleanData(readPcd(strcat('data/',num2str(frame,'%.10d'),'.pcd')));
     %% Iterative procedures
     [R, T, tr, target] = icp(tr, base, target);
-    %% Merge the two point clouds
-    base = target;
+    %% New target
+    base = target;  
+    %% For question 2.1 -- create a merged point cloud with all frames
     base1 = cat(1, base1, target);
+    %% For question 2.2 -- merge the two point clouds
+    base = cat(1, base, target);
 end
-
+%% Show final result
 figure
 p = base1';
 plot3(p(1,:),p(2,:),p(3,:),'b.');
