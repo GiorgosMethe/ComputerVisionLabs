f9eafc24023ec8952349b7ff8f48d1364e4979ad
Paris Mavromoustakos <parismav87@gmail.com>
2014-06-12 21:59:51 +0200

    plots for SFM.
    Amb = with ambiguity. (no correction)

--- src/sfm/main.m
+++ src/sfm/main.m
@@ -11,13 +11,13 @@ addpath('../../data/TeddyBear/');
 addpath('../../data/House/');
 addpath('pvms');
 
-[ pvm , pvmList ] = chaining('House',8, 1000, 1.0);
-save('pvm8.mat','pvm')
-save('pvmList8.mat','pvmList')
+%[ pvm , pvmList ] = chaining('House',8, 1000, 1.0);
+%save('pvm8.mat','pvm')
+%save('pvmList8.mat','pvmList')
 
-%load mat file for teddy bearq
-% load('pvmList1.mat');
-% load('pvm1.mat');
+%load mat file for teddy bear
+ load('pvmList1.mat');
+ load('pvm1.mat');
 
 pvmListImg = mat2gray(pvmList, [0 1]);
 pvmListImg = imresize(pvmListImg, [800 size(pvmList,2)]);
@@ -28,12 +28,12 @@ pvmListImg = imresize(pvmListImg, [800 size(pvmList,2)]);
 %Construct matrix D for sfm
 D = constructD(pvmList, pvm);
 a = sum(pvmList,1);
-indexes = find(a > 47);
+indexes = find(a > 46);
 D = pvm(:,indexes);
 [M, S] = sfm(D);
 
 figure
-plot3(S(1,:),S(2,:),S(3,:),'b.');
+plot3(S(1,:),S(2,:),S(3,:),'b.', 'markersize', 20);
 
 D = load('D.txt');
 [M, S] = sfm(D);
